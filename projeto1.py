080837974585e9d560a588973322aa85cab25e3f
jorgesalhani <jorge.salhani@usp.br>
2023-10-22 03:43:53 -0300

    Almost done!

--- projeto1.py
+++ projeto1.py
@@ -3,7 +3,7 @@ import numpy as np
 ps.set_navigation_style("free")
 ps.set_up_dir("z_up")
 ps.init()
-ps.look_at(camera_location=(0,0,1), target=(0,1,1))
+ps.look_at(camera_location=(0,-5,1), target=(0,1,1))
 
 def animar_rotacao_y(vertices, angulo):
   R = np.array([
@@ -21,30 +21,41 @@ def process(vertices, dt):
   return vertices
 
 
-def main():
-  vertices_original = np.array([
-    [1,4,0],
-    [-1,4,0],
-    [1,4,1],
-    [-1,4,1]
-  ])
-
-  faces = np.array([
-    [0,1,2],
-    [1,3,2]
-  ])
-  ps.register_point_cloud("my points", np.array([[0,0,1],[0,0,-2]]))
-
-  ps_mesh = ps.register_surface_mesh("my mesh", vertices_original, faces)
+def main(V, F):
+  ps_mesh = ps.register_surface_mesh("my mesh", V, F)
   dt = 0
   def callback():
     
-    nonlocal vertices_original, dt
-    vertices = process(vertices_original, dt)
-    dt +=  0.01
+    nonlocal V, dt
+    vertices = process(V, dt)
+    dt +=  0.03
 
     ps_mesh.update_vertex_positions(vertices)
 
   ps.set_user_callback(callback)
   ps.show()
-main()
+
+
+def verticesFacesObj(arquivo_obj):
+  vertices = []
+  faces = []
+  with open(arquivo_obj, 'r') as dados:
+    for linha in dados.readlines():
+      linha = linha.split()
+      if len(linha) == 0: continue
+      if linha[0] == 'v':
+        v = [float(i) for i in linha[1:]]
+        vertices.append(v)
+      elif linha[0] == 'f':
+        f = [int(i)-1 for i in linha[1:]]
+        faces.append(f)
+  return {'vertices': np.array(vertices), 'faces': np.array(faces)}
+
+
+ps.register_point_cloud("my points", np.array([[0,-100,0], [0,100,0],[-100, -100,-200]]))
+obj = verticesFacesObj('./objFiles/teapot.obj')
+vertices = obj['vertices']
+faces = obj['faces']
+# print(vertices)
+# print(faces)
+main(vertices, faces)
